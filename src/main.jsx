b45e4511a6a335e72e8cb28cfc39ae42b89701c0
Mykhailo <sharkon90@gmail.com>
2024-02-01 09:18:20 -0500

    adding routes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+// importing all components to render when hitting different roads
+import App from './App.jsx';
+import About from './components/About-Me';
+import Portfolio from './components/Portfolio';
+import Error from './components/Error';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+
+// adding router to listen different routs on Web app
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
