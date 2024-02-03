381b95a55b54c4be86155fbeabb04ed6e8fa197c
Mykhailo <sharkon90@gmail.com>
2024-02-03 13:17:35 -0500

    adding Resume screen

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -1,3 +1,16 @@
-export default function About() {
-    return <div></div>
+import "./resume.css";
+export default function Resume() {
+  return (
+    <section>
+      <h2>Resume</h2>
+      <object className="pdf" type="application/pdf" data="/public\resume.pdf">
+        <p>
+          This browser does not support PDFs. Please download the PDF to view
+          <a href="/public\resume.pdf" download="Mykhailo-Profile">
+            Download PDF
+          </a>
+        </p>
+      </object>
+    </section>
+  );
 }
