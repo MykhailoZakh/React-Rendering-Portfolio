8ddf380856cc54a1a20c8b70b9b0a79f52fd3fa9
Mykhailo <sharkon90@gmail.com>
2024-02-05 10:21:18 -0500

    fixing routes for imgs

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,17 +1,21 @@
 import "./footer.css";
+// import '../../../public'
 export default function Footer() {
   return (
     <footer>
       <div className="d-flex flex-row justify-content-center ">
         <a href="https://github.com/MykhailoZakh">
-          <img src="public\github.svg" className="footerIco round"></img>
+          <img
+            src="../../../public\github.svg"
+            className="footerIco round"
+          ></img>
         </a>
         <a href="https://www.linkedin.com/in/mykhailo-zakharkiv-359832286/">
-          <img src="public\linkedin.svg" className="footerIco"></img>
+          <img src="../../../public\linkedin.svg" className="footerIco"></img>
         </a>
         <a href="https://stackoverflow.com/users/22684216/mykhailo-zakharkiv">
           <img
-            src="public\stack-overflow.svg"
+            src="../../../public/stack-overflow.svg"
             className="footerIco round"
           ></img>
         </a>
